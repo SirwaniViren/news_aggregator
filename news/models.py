41f8fa9b0be458f7770cdbdc2fdb0a7a273caea5
SirwaniViren <sirwani.viren@gmail.com>
2021-05-26 01:09:25 +0100

    refactor views

--- news/models.py
+++ news/models.py
@@ -1,13 +1,12 @@
 from django.db import models
 
-
 # Create your models here.
 
 class Article(models.Model):
     title = models.CharField(max_length=200)
-    image = models.URLField(null=True, blank=True)
-    title_url = models.TextField()
-    summary = models.CharField(max_length=1000)
+    image = models.URLField(null=True,blank=True)
+    title_url = models.TextField
+    summary = models.CharField(max_length=1000,null=True,blank=True)
     tag = models.CharField(max_length=100)
 
     def __str__(self):
--- news/views.py
+++ news/views.py
@@ -3,41 +3,57 @@ from bs4 import BeautifulSoup
 from news.models import Article
 import requests
 import re
+import pandas as pd
 
 
 requests.packages.urllib3.disable_warnings()
 # Create your views here.
-def getArticle(request):
-    url = "https://www.bbc.com/"
+def scrape():
+    titles = []
+    urls = []
+    images = []
+    tags = []
+    summaries = []
+    url = "https://www.bbc.com"
     headers = { "User-Agent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36'}
     r = requests.get(url=url,headers=headers)
     soup = BeautifulSoup(r.content,'html.parser')
 
     articles = soup.find_all('li', class_=re.compile('^media-list__item media-list__item'))
     for piece in articles:
-        new_piece = Article()
         if piece.find('a',{'class':'media__link'}) is not None:
-            new_piece.title = piece.find('a',{'class':'media__link'}).get_text()
+            titles.append(piece.find('a',{'class':'media__link'}).get_text().strip())
             link_url = piece.find('a',{'class':'media__link'})['href']
             if link_url.startswith('https'):
-                new_piece.title_url = str(piece.find('a',{'class':'media__link'})['href'])
+                urls.append(str(piece.find('a',{'class':'media__link'})['href']))
             else:
-                new_piece.title_url = url + str(piece.find('a',{'class':'media__link'})['href'])
+                urls.append(url + str(piece.find('a',{'class':'media__link'})['href']))
             image = piece.find('img')['src']
-            if image is not None:
-                new_piece.image = str(image)
+            if image is not None and not image.startswith("data"):
+                images.append(str(image))
             else:
-                new_piece.image = 'https://martialartsplusinc.com/wp-content/uploads/2017/04/default-image-620x600.jpg'
+                images.append('https://martialartsplusinc.com/wp-content/uploads/2017/04/default-image-620x600.jpg')
             summary = piece.find('p',{'class':'media__summary'})
             if summary is None:
-                new_piece.summary = "no summary"
+                summaries.append("no summary")
             else:
-                new_piece.summary = summary.get_text()
+                summaries.append(summary.get_text().strip())
             tag = piece.find('a',{'class':'media__tag tag tag--news'})
             if tag is not None:
-                new_piece.tag = tag.get_text()
+                tags.append(tag.get_text().strip())
             else:
-                new_piece.tag = "N/A"
+                tags.append("N/A")
+                
+    bbc_frame = pd.DataFrame({"title":titles, "url":urls, "image":images,"summary":summaries,"tag":tags})
+    return bbc_frame
+
+def getArticle():
+    frame = scrape()
+    for i in range(len(frame)):
+        new_piece = Article()
+        new_piece.title = str(frame.loc[i][0])
+        new_piece.title_url = frame.loc[i][1]
+        new_piece.image = str(frame.loc[i][2])
         new_piece.save()
     return redirect("../")
 
